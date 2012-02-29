40c05fd58c310daa85c015e8470540cd51cb0fd1
Lowell Kirsh <lowell@twist.com>
2012-02-29 14:14:51 -0800

    add pause/resume and battery event handlers

--- assets/www/main.js
+++ assets/www/main.js
@@ -1,3 +1,34 @@
+var onPause = function() {
+  console.log('Pause');
+};
+
+var onResume = function() {
+  console.log('Resume');
+};
+
+var onBatteryStatus = function(e) {
+  console.log("Level: " + e.level);
+};
+
+var onBatteryLow = function(e) {
+  console.log("Level: " + e.level);
+};
+
+var onBatteryCritical = function(e) {
+  console.log("Level: " + e.level);
+};
+
+var onDeviceReady = function() {
+  document.addEventListener('pause', onPause, false);
+  document.addEventListener('resume', onResume, false);
+
+  // http://docs.phonegap.com/en/1.3.0/phonegap_events_events.md.html#batterystatus
+  window.addEventListener('batterystatus', onBatteryStatus, false);
+  window.addEventListener('batterylow', onBatteryLow, false);
+  window.addEventListener('batterycritical', onBatteryCritical, false);
+};
+
 $(function() {
-  console.log('Hello world!');
+  console.log('Init');
+  document.addEventListener('deviceready', onDeviceReady, false);
 });
